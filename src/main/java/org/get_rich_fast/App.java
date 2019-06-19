27581a4a2a39cd84c8044a0b6248f1e173746d0f
Jab1748 <jan1748@schule.bremen.de>
2019-06-19 16:03:24 +0200

    Create Projekt

---
+++ src/main/java/org/get_rich_fast/App.java
@@ -0,0 +1,13 @@
+package org.get_rich_fast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
