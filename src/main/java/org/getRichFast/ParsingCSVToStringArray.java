bbd1e977a16fcd46650ba6d71e8397af0d0cd520
Jab1748 <jan1748@schule.bremen.de>
2019-06-20 15:57:04 +0200

    Parsing Date and Big Decimal

--- src/main/java/org/getRichFast/ParsingCSVToStringArray.java
+++ src/main/java/org/getRichFast/ParsingCSVToStringArray.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class ParsingCSVToStringArray {
 
-  private String[] parsingStringLine(String line) {
+  private String[] splitingStringLine(String line) {
     String[] splited = line.split(",");
     return splited;
   }
@@ -12,7 +12,7 @@ public class ParsingCSVToStringArray {
   public ArrayList<String[]> getDataArrayList(ArrayList<String> lines) {
     ArrayList<String[]> data = new ArrayList<>();
     for (int i = 0; i < lines.size(); i++) {
-      data.add(parsingStringLine(lines.get(i)));
+      data.add(splitingStringLine(lines.get(i)));
     }
     return data;
   }
