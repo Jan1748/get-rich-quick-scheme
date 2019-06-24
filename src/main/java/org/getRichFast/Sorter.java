07d587aedce5f6aea4cd10b3fa2241e7b429817e
Jab1748 <jan1748@schule.bremen.de>
2019-06-24 16:02:04 +0200

    Sorter Not ready

--- src/main/java/org/getRichFast/Sorter.java
+++ src/main/java/org/getRichFast/Sorter.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Sorter {
   private ArrayList<StockBuild> stockFounds;
+  private BigDecimal lowest;
 
   public Sorter(ArrayList<StockBuild> stockFounds){
     this.stockFounds = stockFounds;
@@ -13,15 +14,14 @@ public class Sorter {
     for(int i = 0; i < stockFounds.size(); i++){
       StockBuild stock = stockFounds.get(i);
       switch (choice){
-        case "Open":stock.getOpen();break;
+        case "Open":if (compare(stock.getOpen()));break;
         case "Low":stock.getLow();break;
         case "High":stock.getHigh();break;
         case "Close":stock.getClose();break;
       }
     }
   }
-  private void compare(BigDecimal input){
-    //if(input < )
+  private Boolean compare(BigDecimal input){
 
   }
 
