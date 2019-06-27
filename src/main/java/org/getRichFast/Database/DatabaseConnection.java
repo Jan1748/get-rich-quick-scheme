e2d808cb863bf68c48e55e2929338e871f7824b6
leonard <leonard53@schule.brmenden.de>
2019-06-27 09:46:58 +0200

    Test with JDBC and PostgreSQL

--- src/main/java/org/getRichFast/Database/DatabaseConnection.java
+++ src/main/java/org/getRichFast/Database/DatabaseConnection.java
@@ -8,29 +8,32 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnection {
-    public void connect(){
-      try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/example", "postgres", "postgres")) {
 
-        System.out.println("Java JDBC PostgreSQL Example");
-        // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
-        // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
+  public void connect() {
+    try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Test", "postgres", "1234")) {
+
+      System.out.println("Java JDBC PostgreSQL Example");
+      // When this class first attempts to establish a connection, it automatically loads any JDBC 4.0 drivers found within
+      // the class path. Note that your application must manually load any JDBC drivers prior to version 4.0.
 //          Class.forName("org.postgresql.Driver");
 
-        System.out.println("Connected to PostgreSQL database!");
-        Statement statement = connection.createStatement();
-        System.out.println("Reading car records...");
-        System.out.printf("%-30.30s  %-30.30s%n", "Model", "Price");
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM public.cars");
-        while (resultSet.next()) {
-          System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("model"), resultSet.getString("price"));
-        }
+      System.out.println("Connected to PostgreSQL database!");
+      Statement statement = connection.createStatement();
 
-      } /*catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC driver not found.");
-            e.printStackTrace();
-        }*/ catch (SQLException e) {
-        System.out.println("Connection failure.");
-        e.printStackTrace();
+      String sql = "INSERT INTO stockbuild " + "VALUES (6, '2019-06-27', 'FSE/EON_X', 89.5, 90.4, 89.5, 91.0)";
+      statement.executeUpdate(sql);
+
+      System.out.printf("%-10.10s  %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s%n", "datum", "symbol", "open", "high", "low", "close");
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM public.stockbuild");
+      while (resultSet.next()) {
+        System.out
+            .printf("%-10.10s  %-10.10s %-10.10s %-10.10s %-10.10s %-10.10s%n", resultSet.getString("datum"), resultSet.getString("symbol"), resultSet.getString("open"), resultSet.getString("high"),
+                resultSet.getString("low"), resultSet.getString("close"));
       }
+
+    } catch (SQLException e) {
+      System.out.println("Connection failure.");
+      e.printStackTrace();
     }
+  }
 }
--- src/main/java/org/getRichFast/Main.java
+++ src/main/java/org/getRichFast/Main.java
@@ -1,11 +1,14 @@
 package org.getRichFast;
 
+import org.getRichFast.Database.DatabaseConnection;
 import org.getRichFast.UI.Menus;
 
 public class Main {
 
   public static void main(String[] args) {
     Menus menus = new Menus();
-    menus.startMenu();
+    //menus.startMenu();
+    DatabaseConnection databaseConnection = new DatabaseConnection();
+    databaseConnection.connect();
   }
 }
