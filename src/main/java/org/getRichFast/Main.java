b64116912d18436e88fb76d2397f98d09587763e
Jab1748 <jan1748@schule.bremen.de>
2019-06-27 10:51:10 +0200

    Database package

--- src/main/java/org/getRichFast/Main.java
+++ src/main/java/org/getRichFast/Main.java
@@ -6,6 +6,8 @@ import org.getRichFast.UI.Menus;
 public class Main {
 
   public static void main(String[] args) {
+    DatabaseConnection database = new DatabaseConnection();
+    database.connect();
     Menus menus = new Menus();
     //menus.startMenu();
     DatabaseConnection databaseConnection = new DatabaseConnection();
