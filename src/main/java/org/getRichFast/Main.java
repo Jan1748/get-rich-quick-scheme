8f40a1880817ce646fc0c87eea9a9e0986b72692
Jab1748 <jan1748@schule.bremen.de>
2019-06-28 09:11:30 +0200

    changes

--- src/main/java/org/getRichFast/Main.java
+++ src/main/java/org/getRichFast/Main.java
@@ -1,13 +1,10 @@
 package org.getRichFast;
 
-import org.getRichFast.Database.QueryDatabase;
 import org.getRichFast.UI.Menus;
 
 public class Main {
 
   public static void main(String[] args) {
-    QueryDatabase queryDatabase = new QueryDatabase();
-    queryDatabase.quaryExactDate();
     Menus menus = new Menus();
     menus.startMenu();
   }
