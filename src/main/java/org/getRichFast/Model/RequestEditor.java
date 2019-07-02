6fee7901a3f3ccf02c67c0d8d3aedaff8481593a
Jab1748 <jan1748@schule.bremen.de>
2019-07-02 14:38:35 +0200

    fixed

--- src/main/java/org/getRichFast/Model/RequestEditor.java
+++ src/main/java/org/getRichFast/Model/RequestEditor.java
@@ -3,14 +3,14 @@ package org.getRichFast.Model;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
-import org.getRichFast.Data.Database.ModelToDatabaseConnection;
+import org.getRichFast.Data.Database.DatabaseConnection;
 import org.getRichFast.Model.Downloading.QuandlCodeFinder;
 import org.getRichFast.Model.Entity.DataShifter;
 import org.getRichFast.Model.Entity.StockBuild;
 
 public class RequestEditor {
 
-  private ModelToDatabaseConnection databaseConnection = new ModelToDatabaseConnection();
+  private DatabaseConnection databaseConnection = new DatabaseConnection();
 
   public void downloadQuandlWholeStockMarket(String symbol, String apiKey) {
     ArrayList<String> symbols = null;
