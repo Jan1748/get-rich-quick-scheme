fb33012cef7b5de9fa60fa0e9d0330e341b4eb4c
Jab1748 <jan1748@schule.bremen.de>
2019-07-03 17:21:57 +0200

    JfreeChart Test LineChart added

--- src/main/java/org/getRichFast/Main.java
+++ src/main/java/org/getRichFast/Main.java
@@ -1,10 +1,12 @@
 package org.getRichFast;
 
+import org.getRichFast.Model.Charts.CreateLineChart;
 import org.getRichFast.UI.ConsoleUI.Menus;
 
 public class Main {
-
   public static void main(String[] args) {
+    CreateLineChart createLineChart = new CreateLineChart();
+    createLineChart.LineChart_AWT();
     Menus menus = new Menus();
     menus.startMenu();
   }
---
+++ src/main/java/org/getRichFast/Model/Charts/CreateLineChart.java
@@ -0,0 +1,35 @@
+package org.getRichFast.Model.Charts;
+
+import java.io.File;
+import java.io.IOException;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+
+
+public class CreateLineChart {
+  public void LineChart_AWT() {
+
+    //Prepare the data set
+    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+    dataset.addValue( 15 , "schools" , "1970" );
+    dataset.addValue( 30 , "schools" , "1980" );
+    dataset.addValue( 60 , "schools" ,  "1990" );
+    dataset.addValue( 120 , "schools" , "2000" );
+    dataset.addValue( 240 , "schools" , "2010" );
+    dataset.addValue( 300 , "schools" , "2014" );
+
+    //Create the chart
+    JFreeChart chart = ChartFactory.createLineChart("Gaming", "Years","Number of Schools", dataset,PlotOrientation.VERTICAL,true,true,false);
+
+    //Save chart as PNG
+    try {
+      ChartUtilities.saveChartAsPNG(new File("soft3d.png"), chart, 400, 300);
+    } catch (IOException e) {
+      System.out.println("ERROR");
+    }
+  }
+}
