d0a253de392d3c513c57e5b6f66ad779eb14e368
Jab1748 <jan1748@schule.bremen.de>
2019-07-04 14:45:17 +0200

    For Leofri3

--- src/main/java/org/getRichFast/Model/Charts/CreateLineChartPNG.java
+++ src/main/java/org/getRichFast/Model/Charts/CreateLineChartPNG.java
@@ -1,36 +1,34 @@
 package org.getRichFast.Model.Charts;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import org.getRichFast.Model.Entity.StockBuild;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CategoryAxis;
-import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.jdbc.JDBCCategoryDataset;
 
 
 public class CreateLineChartPNG {
-  private DefaultCategoryDataset dataset;
-  public void generateChartPNG(ArrayList<StockBuild> data) {
+  private ArrayList<StockBuild> data;
+  public void generateChartPNG(ArrayList<StockBuild> input) {
+
+    data = input;
+    System.out.println("Data length " + data.size());
     StockBuild stockBuild = data.get(0);
-    for(int i = 0; i < data.size(); i++){
-      stockBuild = data.get(i);
-      DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-      Date dateTest = stockBuild.getDate().getTime();
-      String dateString = dateFormat.format(dateTest);
-      System.out.println("Date " + stockBuild.getDate());
-      dataset.addValue(stockBuild.getOpen(), "OPEN", dateString);
-    }
-    JFreeChart chart = ChartFactory.createLineChart( stockBuild.getSymbol(),"Time","Value", dataset,PlotOrientation.VERTICAL,false,false,false);
+    DefaultCategoryDataset dataset = createDataset(stockBuild.getSymbol());
+    System.out.println(dataset.getColumnKeys());
+    JFreeChart chart = ChartFactory.createLineChart(
+        stockBuild.getSymbol(),
+        "Years","Number of Schools",
+        dataset,
+        PlotOrientation.VERTICAL,
+        true,true,false);
+
 
     try {
       ChartUtils.saveChartAsPNG(new File("LineChart.png"), chart, 800, 600);
@@ -38,5 +36,22 @@ public class CreateLineChartPNG {
     } catch (Exception e) {
       System.out.println("ERROR");
     }
+  }
+  private DefaultCategoryDataset createDataset(String symbol) {
+    DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+    //for(int i = 0; i < data.size(); i++) {
+      StockBuild stockBuild = data.get(0);
+      DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+      Date dateTest = stockBuild.getDate().getTime();
+      String dateString = dateFormat.format(dateTest);
+      dataset.setValue(1, "Open", dateString);
+    stockBuild = data.get(1);
+      dataset.addValue(2, "Open", dateString);
+    stockBuild = data.get(2);
+      dataset.addValue(3, "Open", dateString);
+
+    //}
+    return dataset;
   }
 }
